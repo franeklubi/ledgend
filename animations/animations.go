0eb33b1cfdfa78a1653b79911b8145b680a29b84
kowmat <40365411+kowmat@users.noreply.github.com>
2019-10-23 22:39:56 +0200

    Add Pulse animation
    :)

--- animations/animations.go
+++ animations/animations.go
@@ -37,3 +37,35 @@ func FromMiddleFullSweep(
 
     return a, b
 }
+
+
+func Pulse(
+    direction bool,
+    start_pos, length float64,
+    start_colour_a, end_colour_a Color,
+    start_colour_b, end_colour_b Color,
+    duration, duration_back time.Duration,
+) (Animation, Animation) {
+    a := Sweep(
+        direction, start_pos, length,
+        start_colour_a, end_colour_a,
+        duration,
+    )
+
+
+    b_start_pos := start_pos + (length * (1 - start_pos))
+    b_length := 1-start_pos
+    if (!direction) {
+        b_length = b_start_pos
+        b_start_pos = 1 - b_start_pos
+    }
+
+    b := Sweep(
+        !direction, b_start_pos, b_length,
+        start_colour_b, end_colour_b,
+        duration_back,
+    )
+    b.Start = b.Start.Add(duration)
+
+    return a, b
+}
